1515de3dd81c7ea9fc26a8f625f9524beef0ecfe
Gregory Reneris <greg@reneris.com>
2020-01-20 15:51:56 -0800

    fixed config pw

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 "use strict";
-
+console.log ("-------------------------arrived----------------------------")
 var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
